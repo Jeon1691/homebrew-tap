1d1bdf5060fe01f52079ca8c016311e7d69540a2
clipsync-release <clipsync@users.noreply.github.com>
2026-09-16 13:59:34 +0000

    clipsync v0.1.2

--- Formula/clipsync.rb
+++ Formula/clipsync.rb
@@ -1,7 +1,7 @@
 class Clipsync < Formula
   desc "End-to-end encrypted multi-device clipboard sync"
   homepage "https://github.com/Jeon1691/clipsync"
-  version "0.1.1"
+  version "0.1.2"
   license any_of: ["MIT", "Apache-2.0"]
 
   livecheck do
@@ -11,21 +11,21 @@ class Clipsync < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Jeon1691/clipsync/releases/download/v0.1.1/clipsync-aarch64-apple-darwin.tar.gz"
-      sha256 "819b6e718dae39f47e3a3e293710da6f7fe890c5b79bc6fcc34ff227e7bb9f66"
+      url "https://github.com/Jeon1691/clipsync/releases/download/v0.1.2/clipsync-aarch64-apple-darwin.tar.gz"
+      sha256 "7ffb8e4a5c3524622011fe2bfea20df6e7f5d077e73e4f80edc86ebefd57c0f6"
     else
-      url "https://github.com/Jeon1691/clipsync/releases/download/v0.1.1/clipsync-x86_64-apple-darwin.tar.gz"
-      sha256 "57e5de876602f4c9804f5d8339ad5d342ef434792bed0a8e5a644e2c2a55c6be"
+      url "https://github.com/Jeon1691/clipsync/releases/download/v0.1.2/clipsync-x86_64-apple-darwin.tar.gz"
+      sha256 "36cac1b9bd882945b888893aa3ce7971afc5de60f6d23492afcf388dcb9186b7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Jeon1691/clipsync/releases/download/v0.1.1/clipsync-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "25f23d77884f089d8214198d01dcf16124043ed75fa4144af6cb997af2175024"
+      url "https://github.com/Jeon1691/clipsync/releases/download/v0.1.2/clipsync-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "c3a77d8cd55e9494f9298de6bc5f7e5f9470c494f0a561f7d33a41037b60ea87"
     else
-      url "https://github.com/Jeon1691/clipsync/releases/download/v0.1.1/clipsync-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c8d112805d2ef82186c54a6670e89bad7d71c377f134ea72673f7e6926768f10"
+      url "https://github.com/Jeon1691/clipsync/releases/download/v0.1.2/clipsync-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "02c674998efab82bb6710e17d4ddfadc74f188bda83406d57c7622bd0a92a879"
     end
   end
 
